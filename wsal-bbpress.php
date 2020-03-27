574db994b3f667cc05608c226296224c2f5c0e4a
Danny Jones <59394454+DannyWPWS@users.noreply.github.com>
2020-03-27 18:02:35 +0000

    Adding meta formatting

--- wsal-bbpress.php
+++ wsal-bbpress.php
@@ -169,4 +169,31 @@ function wsal_bbpress_add_custom_event_objects( $objects ) {
 	return $objects;
 }
 
-add_filter( 'wsal_event_objects', 'wsal_bbpress_add_custom_event_objects' );
+/**
+ * Adds new meta formatting for our plugion
+ *
+ * @method wsal_wpforms_add_custom_meta_format
+ * @since  1.0.0
+ */
+function wsal_bbpress_add_custom_meta_format( $value, $name ) {
+	$check_value = (string) $value;
+  if ( '%EditorLinkForum%' === $name ) {
+		if ( 'NULL' !== $check_value ) {
+			return '<a target="_blank" href="' . esc_url( $value ) . '">' . __( 'View the Forum in editor', 'wp-security-audit-log' ) . '</a>';
+		} else {
+			return '';
+		}
+	}
+  if ( '%EditorLinkTopic%' === $name ) {
+		if ( 'NULL' !== $check_value ) {
+			  return '<a target="_blank" href="' . esc_url( $value ) . '">' . __( 'View the Topic in editor', 'wp-security-audit-log' ) . '</a>';
+		} else {
+			return '';
+		}
+	}
+	return $value;
+}
+
+add_filter( 'wsal_event_objects', 'wsal_bbpress_add_custom_event_objects', 10, 2 );
+add_filter( 'wsal_link_filter', 'wsal_bbpress_add_custom_meta_format', 10, 2 );
+add_filter( 'wsal_meta_formatter_custom_formatter', 'wsal_bbpress_add_custom_meta_format', 10, 2 );
