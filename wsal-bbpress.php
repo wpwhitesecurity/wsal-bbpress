9ce6fe63c20a3a11dfbd9421b311aa84efe8007b
Danny Jones <59394454+DannyWPWS@users.noreply.github.com>
2020-05-12 17:35:26 +0100

    Fixing typos

--- wsal-bbpress.php
+++ wsal-bbpress.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: WP Activity Log for for bbPress
+ * Plugin Name: WP Activity Log for bbPress
  * Plugin URI: https://wpactivitylog.com/
  * Description: An extension to the WP Activity Log Plugin to track events within the bbPress plugin.
  * Text Domain: wp-security-audit-log
